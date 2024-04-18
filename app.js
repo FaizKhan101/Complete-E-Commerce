0c3bb8f093fd491e0aa34d4a734cda032b0e3e55
Faiz <faizkhan8225@gmail.com>
2024-04-18 11:22:59 +0530

    Create folders, files and a first set of routes (auth routes)

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express")
 
+const authRoutes = require("./routes/auth.routes")
+
 const app = express()
+
+app.use("/auth", authRoutes)
 
 app.listen(3000, () => console.log("Server start at port: 3000."))
