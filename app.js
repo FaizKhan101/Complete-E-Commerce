ecdfaa05bd98bc2b87188bd77f18ebd0b75a5da2
Faiz <faizkhan8225@gmail.com>
2024-04-18 14:04:26 +0530

    Adding a mongoDb database server and connection

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-const path = require("path")
+const path = require("path");
 
-const express = require("express")
+const express = require("express");
 
-const authRoutes = require("./routes/auth.routes")
+const db = require("./data/database");
+const authRoutes = require("./routes/auth.routes");
 
-const app = express()
+const app = express();
 
-app.set("view engine", "ejs")
-app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
-app.use("/auth", authRoutes)
+app.use("/auth", authRoutes);
 
-app.listen(3000, () => console.log("Server start at port: 3000."))
+db.connectToDb()
+  .then(() => {
+    app.listen(3000, () => console.log("Server start at port: 3000."));
+  })
+  .catch((err) => {
+    console.log("Failed to connect to the database.");
+    console.log(err);
+  });
---
+++ data/database.js
@@ -0,0 +1,23 @@
+const mongodb = require("mongodb")
+
+const MongoClient = mongodb.MongoClient
+
+let _db;
+
+const connectToDb = async () => {
+    const client = await MongoClient.connect("mongodb://localhost:27017")
+    _db =client.db("online-shop")
+    console.log("Connected!");
+}
+
+const getDb = () => {
+    if (!_id) {
+        throw new Error("COnnection to the database failed!")
+    } 
+    return _db
+}
+
+module.exports = {
+    connectToDb: connectToDb,
+    getDb: getDb
+}
